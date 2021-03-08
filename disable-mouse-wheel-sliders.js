a545f3c4dcdb6dd9b02c7a91e9d5bb196533a9b2
mrkwnzl <marko.wenzel@me.com>
2021-03-08 11:28:34 +0100

    Update 1.1.0

--- disable-mouse-wheel-sliders.js
+++ disable-mouse-wheel-sliders.js
@@ -3,33 +3,44 @@
 * @param {WheelEvent} event    A Mouse Wheel scroll event
 */
 function _handleMouseWheelInputChange_Override(event) {
-  if (game.settings.get("disable-mouse-wheel-sliders", "disable-mouse-wheel-sliders")) {
-    // implementation replaced with no-op to avoid scroll wheel changing slider values
-  } else {
-    const r = event.target;
-    if ( (r.tagName !== "INPUT") || (r.type !== "range")) return;
-    event.preventDefault();
-    event.stopPropagation();
+  // implementation replaced with no-op to avoid scroll wheel changing slider values
+}
 
-    // Adjust the range slider by the step size
-    const step = (parseFloat(r.step) || 1.0) * Math.sign(-1 * event.deltaY);
-    r.value = Math.clamped(parseFloat(r.value) + step, parseFloat(r.min), parseFloat(r.max));
-
-    // Dispatch a change event that can bubble upwards to the parent form
-    const ev = new Event("change", {bubbles: true});
-    ev.target = ev.currentTarget = r;
-    r.dispatchEvent(ev);
+function disableInputNumbers(event) {
+  const r = event.target;
+  if(r.tagName === "INPUT" && r.type === "number"){
+    r.blur();
   }
 }
 
 Hooks.on("init", function () {
   game.settings.register("disable-mouse-wheel-sliders", "disable-mouse-wheel-sliders", {
-    name: "Mouse wheel control disabled",
+    name: "Disable Sliders",
     hint: "This disables the mouse wheel control for all sliders (e.g. the volume controls in playlists) on this client.",
     scope: "client",
     config: true,
     default: false,
     type: Boolean,
+    onChange: () => location.reload(),
   });
-  _handleMouseWheelInputChange = _handleMouseWheelInputChange_Override;
+
+  game.settings.register("disable-mouse-wheel-sliders", "disable-mouse-wheel-inputs", {
+    name: "Disable Inputs",
+    hint: "This disables the mouse wheel control for all input fields (e.g. grid units for tokens) on this client.",
+    scope: "client",
+    config: true,
+    default: false,
+    type: Boolean,
+    onChange: () => location.reload(),
+  });
+
+  // Override default Foundry function for sliders
+  if (game.settings.get("disable-mouse-wheel-sliders", "disable-mouse-wheel-sliders")) {
+    window._handleMouseWheelInputChange = _handleMouseWheelInputChange_Override;
+  }
+
+  // Override default HTML function for number inputs
+  if (game.settings.get("disable-mouse-wheel-sliders", "disable-mouse-wheel-inputs")) {
+    document.addEventListener("mousewheel", disableInputNumbers);
+  }
 });
