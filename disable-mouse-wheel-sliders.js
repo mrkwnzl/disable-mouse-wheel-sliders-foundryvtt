5dd06c9f1751729a0c335031b90cf94b6766168c
mrkwnzl <40534855+mrkwnzl@users.noreply.github.com>
2021-08-16 21:14:37 +0200

    Implementing escape button

--- disable-mouse-wheel-sliders.js
+++ disable-mouse-wheel-sliders.js
@@ -2,15 +2,41 @@
 * Disable mousewheel control for range type input elements
 * @param {WheelEvent} event    A Mouse Wheel scroll event
 */
+
+function getMetaKey() {
+  if (game.settings.get("disable-mouse-wheel-sliders", "metaKey") == "a") return true;
+  if (game.settings.get("disable-mouse-wheel-sliders", "metaKey") == "b") return event.altKey;
+  if (game.settings.get("disable-mouse-wheel-sliders", "metaKey") == "c") return event.ctrlKey || event.metaKey;
+}
+
 function _handleMouseWheelInputChange_Override(event) {
-  // implementation replaced with no-op to avoid scroll wheel changing slider values
+  let metaKey = getMetaKey();
+  if (!metaKey) {
+    // implementation replaced with no-op to avoid scroll wheel changing slider values
+  } else {
+    const r = event.target;
+    if ( (r.tagName !== "INPUT") || (r.type !== "range") || r.disabled ) return;
+    event.preventDefault();
+    event.stopPropagation();
+
+    // Adjust the range slider by the step size
+    const step = (parseFloat(r.step) || 1.0) * Math.sign(-1 * event.deltaY);
+    r.value = Math.clamped(parseFloat(r.value) + step, parseFloat(r.min), parseFloat(r.max));
+
+    // Dispatch a change event that can bubble upwards to the parent form
+    const ev = new Event("change", {bubbles: true});
+    r.dispatchEvent(ev);
+  }
 }
 
 function disableInputNumbers(event) {
-  const r = event.target;
-  if (r.tagName === "INPUT" && r.type === "number") {
-    r.blur();
-    return false;
+  let metaKey = getMetaKey();
+  if (!metaKey) {
+    const r = event.target;
+    if (r.tagName === "INPUT" && r.type === "number") {
+      r.blur();
+      return false;
+    }
   }
 }
 
@@ -32,6 +58,21 @@ Hooks.on("init", function () {
     config: true,
     default: false,
     type: Boolean,
+    onChange: () => location.reload(),
+  });
+
+  game.settings.register("disable-mouse-wheel-sliders", "metaKey", {
+    name: "Choose Escape Key",
+    hint: "This defines an escape key. While holding this key, mouse wheel control for sliders and input fields is enabled.",
+    scope: "client",
+    config: true,
+    default: "a",
+    type: String,
+    choices: {
+      "a": "none",
+      "b": "alt/option",
+      "c": "ctrl/cmd"
+    },
     onChange: () => location.reload(),
   });
 
